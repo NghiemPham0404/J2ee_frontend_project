b396ea4cb9639a87820a6df5b735860f847f2b3c
XiuQuy <thieuhuy.90603@gmail.com>
2024-10-30 20:36:40 +0700

    header

--- src/main/java/com/example/j2ee_frontend_test/controller/HomeController.java
+++ src/main/java/com/example/j2ee_frontend_test/controller/HomeController.java
@@ -2,17 +2,14 @@ package com.example.j2ee_frontend_test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(Model model) {
-        // Thêm dữ liệu động nếu cần thiết
-        model.addAttribute("title", "Trang Chủ - Dự Án Từ Thiện");
-
-        // Trả về tên của template Thymeleaf (không cần thêm .html)
-        return "home"; // Tên của file Thymeleaf là home.html
+    public String home(Model model, HttpServletRequest request) {
+        model.addAttribute("currentUri", request.getRequestURI());
+        return "home";
     }
 
 }
