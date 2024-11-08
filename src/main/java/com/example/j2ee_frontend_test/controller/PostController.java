3f8096ba86a59b143d7eb2c88071b575960ef1ed
trongphu567 <trongphu543210@gmail.com>
2024-11-08 22:38:37 +0700

    update 8/11
    + tạo trang thêm bài viết mới
    + thêm trình chỉnh sửa nội dung ToastUI
    + thêm route cho posts

--- src/main/java/com/example/j2ee_frontend_test/controller/PostController.java
+++ src/main/java/com/example/j2ee_frontend_test/controller/PostController.java
@@ -1,15 +1,25 @@
 package com.example.j2ee_frontend_test.controller;
 
+import com.example.j2ee_frontend_test.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping("/posts")
+    @GetMapping
     public String showPostManagerPage(Model model) {
         return "post"; // Trả về tên trang HTML (postManager.html)
+    }
+
+    @GetMapping("/new")
+    public String showNewPostPage(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "create_post";
     }
 }
 
