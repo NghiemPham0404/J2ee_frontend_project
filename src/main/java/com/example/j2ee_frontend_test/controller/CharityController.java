f1583a7edd134ca50b2e0824e5e1c7aaf708981e
XiuQuy <thieuhuy.90603@gmail.com>
2024-11-13 22:00:43 +0700

    create charity

--- src/main/java/com/example/j2ee_frontend_test/controller/CharityController.java
+++ src/main/java/com/example/j2ee_frontend_test/controller/CharityController.java
@@ -1,14 +1,26 @@
 package com.example.j2ee_frontend_test.controller;
 
+import com.example.j2ee_frontend_test.models.CharityEvent;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.event.ChangeEvent;
 
 @Controller
+@RequestMapping("/charities")
 public class CharityController {
 
-    @GetMapping("/charities")
+    @GetMapping
     public String showProjectsPage() {
         return "charity";
+    }
+    @GetMapping("/new")
+    public String showNewCharityPage(Model model) {
+        CharityEvent c= new CharityEvent();
+        model.addAttribute("charity",c);
+        return "create_charity";
     }
 }
 
