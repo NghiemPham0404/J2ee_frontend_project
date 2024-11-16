9ffa96a02fd418b2a3aa93f51b6c0a219b95a4b1
XiuQuy <thieuhuy.90603@gmail.com>
2024-11-16 18:54:39 +0700

    fill data Charity events

--- src/main/java/com/example/j2ee_frontend_test/config/RetrofitClientConfig.java
+++ src/main/java/com/example/j2ee_frontend_test/config/RetrofitClientConfig.java
@@ -1,9 +1,6 @@
 package com.example.j2ee_frontend_test.config;
 
-import com.example.j2ee_frontend_test.services.apis.AccountApi;
-import com.example.j2ee_frontend_test.services.apis.PostApi;
-import com.example.j2ee_frontend_test.services.apis.RoleApi;
-import com.example.j2ee_frontend_test.services.apis.TransactionStatementApi;
+import com.example.j2ee_frontend_test.services.apis.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -56,5 +53,10 @@ public class RetrofitClientConfig {
     @Bean
     public PostApi getPostApi(Retrofit retrofit) {
         return retrofit.create(PostApi.class);
+    }
+
+    @Bean
+    public CharityApi getCharityApi(Retrofit retrofit) {
+        return retrofit.create(CharityApi.class);
     }
 }
--- src/main/java/com/example/j2ee_frontend_test/controller/CharityController.java
+++ src/main/java/com/example/j2ee_frontend_test/controller/CharityController.java
@@ -1,6 +1,10 @@
 package com.example.j2ee_frontend_test.controller;
 
 import com.example.j2ee_frontend_test.models.CharityEvent;
+import com.example.j2ee_frontend_test.response.CharityListResponse;
+import com.example.j2ee_frontend_test.services.CharityService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +16,16 @@ import javax.swing.event.ChangeEvent;
 @RequestMapping("/charities")
 public class CharityController {
 
+    @Autowired
+    CharityService charityService;
     @GetMapping
-    public String showProjectsPage() {
+    public String showProjectsPage(Model model,@PathParam("page") Integer page) {
+        page = page != null ? page - 1: 0;
+        CharityListResponse charityListResponse=charityService.getAllCharities(1,page);
+        model.addAttribute("data", charityListResponse.getCharityList());
+        model.addAttribute("page", page);
+        model.addAttribute("total_pages", charityListResponse.getTotalPages());
+        model.addAttribute("total_results", charityListResponse.getTotalResults());
         return "charity";
     }
     @GetMapping("/new")
