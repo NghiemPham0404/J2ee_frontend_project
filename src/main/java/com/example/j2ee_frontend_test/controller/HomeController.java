d2f16cc546ce83dfc2acf185f29b011635960dff
XiuQuy <thieuhuy.90603@gmail.com>
2024-12-02 21:07:00 +0700

    giao dien Home

--- src/main/java/com/example/j2ee_frontend_test/controller/HomeController.java
+++ src/main/java/com/example/j2ee_frontend_test/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.example.j2ee_frontend_test.controller;
+import com.example.j2ee_frontend_test.models.Post;
 import com.example.j2ee_frontend_test.response.PostListResponse;
 import com.example.j2ee_frontend_test.services.PostService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -20,17 +25,31 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model, HttpServletRequest request) throws JsonProcessingException {
         model.addAttribute("currentUri", request.getRequestURI());
-        int page=0;
-        PostListResponse postListResponse = postService.getAllPostsforuser(0);
+        int totalPages=postService.getAllPostsforuser(0).getTotalPages();
 
-        System.out.println(postListResponse.getPostList());
-        ObjectMapper objectMapper = new ObjectMapper();
-        String charityJson = objectMapper.writeValueAsString(postListResponse);
-        System.out.println(charityJson);
-        model.addAttribute("data", postListResponse.getPostList());
-        model.addAttribute("page", page);
-        model.addAttribute("total_pages", postListResponse.getTotalPages());
-        model.addAttribute("total_results", postListResponse.getTotalResults());
+        List<Post> allPost= new ArrayList<>();
+        for (int page=0;page<totalPages;page++){
+            PostListResponse postListResponse=postService.getAllPostsforuser(page);
+            allPost.addAll(postListResponse.getPostList());
+        }
+        Post post=postService.getPostById(UUID.fromString("6eb66d3f-48bb-4d76-8eae-993d5a2d10b0"));
+        PostListResponse postListResponse=postService.getAllPostsforuser(0);
+        List<Post> data=postListResponse.getPostList();
+        List<Post> done= data.stream().filter(p-> p.getCharityEvent().isDisbursed()==true).collect(Collectors.toList());
+        System.out.println(done);
+        model.addAttribute("ip",post);
+        model.addAttribute("data", data);
+        model.addAttribute("done", done);
+        model.addAttribute("total_pages", totalPages);
         return "home";
+    }
+    @GetMapping("/charities_events")
+    public String charities_events(Model model,@PathParam("page") Integer page){
+        page = page != null ? page : 0;
+        PostListResponse postListResponse=postService.getAllPostsforuser(0);
+        List<Post> data=postListResponse.getPostList();
+        model.addAttribute("data", data);
+        model.addAttribute("page", page);
+        return "charities";
     }
 }
